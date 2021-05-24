a60ee227e1747493627c693ec62f10ce5c50970d
Patrick Copp <pcopp@uoguelph.ca>
2021-05-24 12:10:21 -0400

    Done for now, adding to server

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 import os
 from dotenv import load_dotenv
-from functionality import add, list, clear, remove
 
 load_dotenv()
 grocery_lists = {}
@@ -16,21 +15,36 @@ async def on_message(message):
     print(message.content)
     if not message.content.startswith('!'):
         return
-    if message.content == '!quit':
-        await client.close()
+    # if message.content == '!quit':
+    #     await client.close()
+
     if message.content.startswith('!add '):
         if message.guild.id not in grocery_lists:
             grocery_lists[message.guild.id] = []
-        add(grocery_lists[message.guild.id], message.content)
-    elif message.content == '!list':
-        if message.guild.id not in grocery_lists:
-            grocery_lists[message.guild.id] = []
-        list(grocery_lists[message.guild.id])
-    elif message.content == '!clear':
-        if message.guild.id in grocery_lists:
-            clear(grocery_lists[message.guild.id])
-    elif message.content.startswith('!remove '):
-        if message.guild.id in grocery_lists:
-            remove(grocery_lists[message.guild.id], message.content)
+        grocery_lists[message.guild.id].append(message.content[5:])
+        await message.channel.send('Added {0} to the list!'.format(message.content[5:]))
 
+    elif message.content == '!list':
+        if message.guild.id not in grocery_lists or len(grocery_lists[message.guild.id]) == 0:
+            return
+        list = ''
+        for index,item in enumerate(grocery_lists[message.guild.id]):
+            list = list + str(index + 1)+'. '+item+'\n'
+        await message.channel.send(list)
+
+    elif message.content == '!clear':
+        if message.guild.id not in grocery_lists:
+            return
+        grocery_lists[message.guild.id].clear()
+        await message.channel.send('List cleared!')
+
+    elif message.content.startswith('!remove '):
+        if message.guild.id not in grocery_lists:
+            return
+        item = message.content[8:]
+        if item in grocery_lists[message.guild.id]:
+            grocery_lists[message.guild.id].remove(item)
+            await message.channel.send('{0} removed from list!'.format(message.content[8:]))
+        else:
+            await message.channel.send('{0} is not on the list!'.format(message.content[8:]))
 client.run(os.environ.get("TOKEN"))
